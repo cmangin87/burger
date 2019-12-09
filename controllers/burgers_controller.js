a7575e093c2e3bbafcc9cac441e52368680d6f99
Christopher Mangin <mangin.christopher@gmail.com>
2019-12-09 10:45:57 -0500

    force

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -44,6 +44,7 @@ router.delete("/api/burgers/:id", function(req, res) {
 
   burger.deleteOne(condition, function(result) {
     if (result.changedRows === 0) {
+      //
       // If no rows were changed, then the ID must not exist, so 404.
       return res.status(404).end();
     } else {
